f1a62846c616c0566e889e5b111c84e3f187a02a
modularmechanic <312585472+modularmechanic@users.noreply.github.com>
2026-09-14 13:23:57 +0200

    chore(build): stop hiding the Vite chunk size warning

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
 export default defineConfig({
   server: { host: '0.0.0.0' },
-  build: { target: 'es2022', chunkSizeWarningLimit: 1800 },
+  build: { target: 'es2022' },
 });
